ebd69fe2562e0b3fbaf2563de3dd4d94f3896e88
saisritejakuppa <saisritejakuppa@gmail.com>
2023-12-13 17:25:34 +0530

    cloning repos

---
+++ building.sh
@@ -0,0 +1,14 @@
+# [submodule "src/faceswap/roop"]
+# 	path = src/faceswap/roop
+# 	url = https://github.com/s0md3v/roop.git
+
+
+# [submodule "src/faceswap/wav2lip"]
+# 	path = src/faceswap/wav2lip
+# 	url = https://github.com/Rudrabha/Wav2Lip.git
+
+# [submodule "src/superres/GFPGAN"]	
+# 	path = src/superres/GFPGAN
+# 	url = https://github.com/TencentARC/GFPGAN.git
+
+git submodule update --init --recursive
