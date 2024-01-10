b4d8a932e5e07f72c6b9429c20271540251001b4
saisritejakuppa <saisritejakuppa@gmail.com>
2024-01-10 19:55:40 +0530

    removed no used codes

--- building.sh
+++
@@ -1,4 +0,0 @@
-# git submodule update --init --recursive
-git submodule add https://github.com/s0md3v/roop.git src/faceswap/roop
-git submodule add https://github.com/Rudrabha/Wav2Lip.git src/lipsync/Wav2Lip
-git submodule add https://github.com/TencentARC/GFPGAN.git src/superres/GFPGAN
